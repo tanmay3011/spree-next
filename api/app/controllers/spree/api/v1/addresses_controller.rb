a9b918b71f51f3d38e222a924cb05b38f549e85d
Tanmay Sinha <tanmay3011@gmail.com>
2016-02-13 00:38:00 +0530

    Refactor Api::V1::AddressController, avoid exception raising for better performance and refactor specs

--- api/app/controllers/spree/api/v1/addresses_controller.rb
+++ api/app/controllers/spree/api/v1/addresses_controller.rb
@@ -3,19 +3,18 @@ module Spree
     module V1
       class AddressesController < Spree::Api::BaseController
         before_action :find_order
+        before_action :find_address, only: [:show, :update]
 
         def show
           authorize! :read, @order, order_token
-          @address = find_address
           respond_with(@address)
         end
 
         def update
           authorize! :update, @order, order_token
-          @address = find_address
 
           if @address.update_attributes(address_params)
-            respond_with(@address, :default_template => :show)
+            respond_with(@address, default_template: :show)
           else
             invalid_resource!(@address)
           end
@@ -28,17 +27,17 @@ module Spree
         end
 
         def find_order
-          @order = Spree::Order.find_by!(number: order_id)
+          @order = Spree::Order.find_by(number: order_id)
+          not_found unless @order
         end
 
         def find_address
-          if @order.bill_address_id == params[:id].to_i
+          @address = if @order.bill_address_id == params[:id].to_i
             @order.bill_address
           elsif @order.ship_address_id == params[:id].to_i
             @order.ship_address
-          else
-            raise CanCan::AccessDenied
           end
+          not_found unless @address
         end
       end
     end
